76f7df3214e9f9c04a4b37515b4f96cf1fc6ac60
moklad <dev.moklad@gmail.com>
2025-07-21 15:20:42 +0900

    feat: VC 생성 및 관련 기본 설정

---
+++ SnapKitPractice/JackFlixViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JackFlixViewController.swift
+//  SnapKitPractice
+//
+//  Created by Youngjun Kim on 7/21/25.
+//
+
+import UIKit
+
+class JackFlixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SnapKitPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapKitPractice
+//
+//  Created by Youngjun Kim on 7/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+    
+    
+
+}
+
